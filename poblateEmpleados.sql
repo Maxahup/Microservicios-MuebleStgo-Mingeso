9a1ef2cf078da97450fd093410f0ce4ad7f908f6
Maximiliano Ahumada <64162650+Maxahup@users.noreply.github.com>
2022-11-26 23:55:13 -0300

    Archivos para poblar y crear B.B.D.D
    Archivos para crear las bases de datos de cada microservicio.

---
+++ poblateEmpleados.sql
@@ -0,0 +1,6 @@
+insert into muebleStgo_Personal.personal(rut, nombre, apellidos, anno_Servicios, sueldo_fijo, categoria)
+	values
+	('10234234-4', 'Christopher Robert', 'Evans Evans', 5, 1200000, 'B'),
+	('22222222-2', 'Robert Jhon', 'Downey Jr', 10, 800000, 'C'),
+	('23232323-2', 'Elizabeth Chase', 'Olsen Olsen', 3, 1200000, 'B'),
+	('19191191-1', 'Scarlett Ingrid', 'Johansson Johansson', 8, 1700000, 'A');
